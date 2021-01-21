46c4b19f6238185951366c6c41eb25aa9f69ec7e
Alessandro Capelli <76056603+AlessandroCapelli@users.noreply.github.com>
2021-01-21 20:20:11 +0100

    Create config.py

---
+++ searchengine/config.py
@@ -0,0 +1,10 @@
+CONSUMER_KEY = ''
+CONSUMER_SECRET = ''
+ACCESS_TOKEN = ''
+ACCESS_TOKEN_SECRET = ''
+
+index = 'tweets_index'
+userprofile_index = 'user_profile_index'
+dir = './'
+twitter_users = ['@JoeBiden', '@BarackObama', '@KamalaHarris', '@justinbieber', '@JBALVIN', '@diplo']
+users = ['@joebiden', '@barackobama', '@kamalaharris', '@justinbieber', '@jbalvin', '@diplo']
